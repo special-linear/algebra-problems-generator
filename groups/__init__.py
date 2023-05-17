bb9cff63d17fded15a684082048600057385653b
Andrei Smolensky <andrei.smolensky@gmail.com>
2023-05-17 20:14:44 +0300

    BurnsideGeometric <- prisms and antiprisms

--- groups/__init__.py
+++ groups/__init__.py
@@ -1,4 +1,5 @@
 from .todd_coxeter import ToddCoxeter
 from .abelian import FGAbelianGroup
 from .permutations import PermutationsSubgroups
+from .burnside import BurnsideGeometric
 from .sylow import SylowNotSimple
